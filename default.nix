db53020a8edf2c6944be3d2328eaf83054dd5897
jbgoldman1104 <jbgoldman1104@gmail.com>
2023-09-11 01:17:08 +0900

    "updated on 2023-09-11"

--- default.nix
+++ default.nix
@@ -1,4 +1,3 @@
-let
   # Pinned nixpkgs, deterministic. Last updated: 11-08-2023.
   pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/bb9707ef2ea4a5b749b362d5cf81ada3ded2c53f.tar.gz")) {};
 
